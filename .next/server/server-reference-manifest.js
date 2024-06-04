8fd6288ba45b0d29494ad0424b387f15fcaabb58
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-06-04 19:52:04 +0300

    first commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ReLAMyZNoYESFPQ1k71NQAld09YP0SjM50s2J4EZgOg=\"\n}"
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: 'FinVault',
+  decription: 'A next.js 13 Meta Threads Appplication',
+}
+
+export default function RootLayout({
+  children,
+}:{
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+
+      <body className={inter.className}>
+
+      <Topbar />
+      <main>
+        <LeftSidebar />
+
+        <section className="main-container">
+          <div className="w-full max-w-4xl">
+            {children}
+          </div>
+
+        </section>
+
+
+
+
+        <RightSidebar />
+      </main>
+
+      <Bottombar />
+
+      </body>
+    </html>
+  );
+}
